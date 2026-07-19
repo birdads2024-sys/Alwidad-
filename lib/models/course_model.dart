b5ce88617798e5b406855b6ea10b7a52f83046d9
birdTechnology2 <birdtechnology66@gmail.com>
2026-07-19 18:20:15 +0300

    revert: remove DRM protection changes

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -147,13 +147,11 @@ class VideoQuality {
   final String qualityName; // مثال: 480p, 1080p
   final String hlsUrl;
   final String mp4Url;
-  final bool isDrm;
 
   VideoQuality({
     required this.qualityName,
     required this.hlsUrl,
     required this.mp4Url,
-    this.isDrm = false,
   });
 
   factory VideoQuality.fromMap(Map<String, dynamic> map) {
@@ -161,7 +159,6 @@ class VideoQuality {
       qualityName: map['qualityName'] ?? '',
       hlsUrl: map['hlsUrl'] ?? '',
       mp4Url: map['mp4Url'] ?? '',
-      isDrm: map['isDrm'] ?? false,
     );
   }
 
@@ -170,7 +167,6 @@ class VideoQuality {
       'qualityName': qualityName,
       'hlsUrl': hlsUrl,
       'mp4Url': mp4Url,
-      'isDrm': isDrm,
     };
   }
 }
